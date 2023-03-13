308ea2ca00c08f6696bc7dae87e225dcb7b59499
Julio Rugoloe <juliorugolo@gmail.com>
2023-03-13 15:02:23 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './Pages/Login';
+import Album from './Pages/Album';
+import Favorites from './Pages/Favorites';
+import NotFound from './Pages/NotFound';
+import Profile from './Pages/Profile';
+import ProfileEdit from './Pages/ProfileEdit';
+import Search from './Pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTuness</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/Pages/ProfileEdit.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">Edit</div>
+    );
+  }
+}
+
+export default ProfileEdit;
